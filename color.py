e7875e44c5e5582caa54f0a7a7d4cda9ca9ae3eb
371tti <109227273+371tti@users.noreply.github.com>
2023-02-20 00:19:08 +0900

    Create color.py

---
+++ color.py
@@ -0,0 +1,36 @@
+import os
+import time
+
+
+os.system('color') #windows only
+
+time.sleep(0.1)
+def CTXT(text):
+    text_count = 0
+    text_count_end = 0
+    text_count_HEX = 0
+    text_count_HEX_end = 0
+    while text_count != -1:
+        text_count = text.find('$',text_count)
+        text_count_end = text.find('>',text_count) + 1
+        text_esc = text[text_count:text_count_end]
+        text_esc_old = text_esc
+        if "#" in text_esc :
+            while text_count_HEX != -1:
+                text_count_HEX = text_esc.find('#',text_count_HEX)
+                text_count_HEX += 1
+                text_count_HEX_end = text_count_HEX + 6
+                text_HEX = text_esc[text_count_HEX:text_count_HEX_end]
+                 
+                text_esc = text_esc[:text_count_HEX - 1] + str(tuple(int(text_HEX[i:i+2], 16) for i in (0, 2, 4))).translate(str.maketrans({ ',':':' ,  '(':'',  ')':'' })) + text_esc[text_count_HEX_end:]
+                
+                text_count_HEX = text_esc.find('#',text_count_HEX)
+        text = text.replace(text_esc_old,text_esc.translate(str.maketrans({'$':''    ,   'f':'\033[38;2;'  ,  'b':';48;2;'  ,  ':':';'  ,  '>':'m'  , ' ':''})))
+   
+        
+    else:
+        return text + "\033[00m"
+
+#demo
+
+print(CTXT("$f 0 : 255 : 0 >Hello world!"))
